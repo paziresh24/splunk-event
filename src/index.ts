fbf5822b721f893bf6f538a9bfe417b299d2fce7
Amirhossein <dev.amirhbeigi@gmail.com>
2024-01-22 09:30:02 +0330

    feat: add try/catch

--- src/index.ts
+++ src/index.ts
@@ -20,33 +20,14 @@ export const splunk = {
   create: ({ baseUrl, token, constant }: CreateSplunkTypes) => {
     return {
       sendEvent: async ({ group, type, event }: SendEventSplunkTypes) => {
-        const response = await fetch(`${baseUrl}/services/collector`, {
-          method: "POST",
-          headers: {
-            Authorization: `Splunk ${token}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            sourcetype: "_json",
-            event: {
-              event_group: group,
-              event_type: type,
-              ...constant,
-              ...event,
+        try {
+          const response = await fetch(`${baseUrl}/services/collector`, {
+            method: "POST",
+            headers: {
+              Authorization: `Splunk ${token}`,
+              "Content-Type": "application/json",
             },
-          }),
-        });
-        return response.json();
-      },
-      sendBatchEvent: async ({ group, type, events }: SendBatchEventSplunkTypes) => {
-        const response = await fetch(`${baseUrl}/services/collector`, {
-          method: "POST",
-          headers: {
-            Authorization: `Splunk ${token}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(
-            events.map((event) => ({
+            body: JSON.stringify({
               sourcetype: "_json",
               event: {
                 event_group: group,
@@ -54,10 +35,37 @@ export const splunk = {
                 ...constant,
                 ...event,
               },
-            }))
-          ),
-        });
-        return response.json();
+            }),
+          });
+          return response.json();
+        } catch (error) {
+          return undefined;
+        }
+      },
+      sendBatchEvent: async ({ group, type, events }: SendBatchEventSplunkTypes) => {
+        try {
+          const response = await fetch(`${baseUrl}/services/collector`, {
+            method: "POST",
+            headers: {
+              Authorization: `Splunk ${token}`,
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(
+              events.map((event) => ({
+                sourcetype: "_json",
+                event: {
+                  event_group: group,
+                  event_type: type,
+                  ...constant,
+                  ...event,
+                },
+              }))
+            ),
+          });
+          return response.json();
+        } catch (error) {
+          return undefined;
+        }
       },
     };
   },
